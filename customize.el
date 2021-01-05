3e75026555ffeb3b9e2c69cdf90f2ec86607a7d5
Thor <thorck@protonmail.com>
2021-01-05 23:22:54 +0000

    racer support

--- customize.el
+++ customize.el
@@ -8,7 +8,7 @@
  '(org-agenda-include-diary t)
  '(org-agenda-start-on-weekday nil)
  '(package-selected-packages
-	 '(paredit rainbow-delimiters org-journal org-mobile languagetool popup popwin yasnippet-snippets yas-snippet-snippets yasnippet treemacs lsp-ui lsp-ivy rustic rustic-mode rust-mode lsp-mode flycheck ivy-rich free-keys sx hydra zoom use-package solarized-theme moody minions diff-hl counsel-projectile avy auto-compile))
+	 '(racer paredit rainbow-delimiters org-journal org-mobile languagetool popup popwin yasnippet-snippets yas-snippet-snippets yasnippet treemacs lsp-ui lsp-ivy rustic rustic-mode rust-mode lsp-mode flycheck ivy-rich free-keys sx hydra zoom use-package solarized-theme moody minions diff-hl counsel-projectile avy auto-compile))
  '(rustic-ansi-faces
 	 ["black" "red3" "green3" "yellow3" "sandy brown" "magenta3" "cyan3" "white"]))
 (custom-set-faces
