28b54e3294efbf4a12b26ced50f2f52c43e59c25
Thor <thorck@protonmail.com>
2021-01-06 17:11:00 +0000

    twit mode

--- customize.el
+++ customize.el
@@ -1,3 +1,7 @@
+;; Stuff that I've used "customize" to set lives here. Most of the things that I allow to live here are faces,
+;; otherwise I usually rip the non-face setting stuff into a better more organized file for that setting to live
+;; in.
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -8,7 +12,7 @@
  '(org-agenda-include-diary t)
  '(org-agenda-start-on-weekday nil)
  '(package-selected-packages
-	 '(racer paredit rainbow-delimiters org-journal org-mobile languagetool popup popwin yasnippet-snippets yas-snippet-snippets yasnippet treemacs lsp-ui lsp-ivy rustic rustic-mode rust-mode lsp-mode flycheck ivy-rich free-keys sx hydra zoom use-package solarized-theme moody minions diff-hl counsel-projectile avy auto-compile))
+	 '(twittering-mode racer paredit rainbow-delimiters org-journal org-mobile languagetool popup popwin yasnippet-snippets yas-snippet-snippets yasnippet treemacs lsp-ui lsp-ivy rustic rustic-mode rust-mode lsp-mode flycheck ivy-rich free-keys sx hydra zoom use-package solarized-theme moody minions diff-hl counsel-projectile avy auto-compile))
  '(rustic-ansi-faces
 	 ["black" "red3" "green3" "yellow3" "sandy brown" "magenta3" "cyan3" "white"]))
 (custom-set-faces
