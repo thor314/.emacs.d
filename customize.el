0f89ee7c4293ec556d801b733bd2d6ea678820a6
Thor <thorck@protonmail.com>
2021-01-17 14:03:20 +0000

    polymode

--- customize.el
+++ customize.el
@@ -7,12 +7,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/org/agenda"))
+ '(cargo-process--command-current-file-tests "test")
+ '(cargo-process--command-current-test "test")
+ '(cargo-process--command-test "test")
+ '(org-agenda-files
+	 '("/home/thor/org/agenda/dev.org" "/home/thor/org/agenda/emacs.org" "/home/thor/org/agenda/mintbase.org" "/home/thor/org/agenda/misc.org" "/home/thor/org/agenda/pers.org" "/home/thor/org/journal/20210114"))
  '(org-agenda-include-deadlines t)
  '(org-agenda-include-diary t)
+ '(org-agenda-restore-windows-after-quit t)
  '(org-agenda-start-on-weekday nil)
+ '(org-agenda-todo-ignore-deadlines 'far)
+ '(org-journal-enable-agenda-integration t)
  '(package-selected-packages
-	 '(twittering-mode racer paredit rainbow-delimiters org-journal org-mobile languagetool popup popwin yasnippet-snippets yas-snippet-snippets yasnippet treemacs lsp-ui lsp-ivy rustic rustic-mode rust-mode lsp-mode flycheck ivy-rich free-keys sx hydra zoom use-package solarized-theme moody minions diff-hl counsel-projectile avy auto-compile))
+	 '(poly-markdown poly-org polymode twittering-mode racer paredit rainbow-delimiters org-journal org-mobile languagetool popup popwin yasnippet-snippets yas-snippet-snippets yasnippet treemacs lsp-ui lsp-ivy rustic rustic-mode rust-mode lsp-mode flycheck ivy-rich free-keys sx hydra zoom use-package solarized-theme moody minions diff-hl counsel-projectile avy auto-compile))
+ '(ring-bell-function 'ignore)
  '(rustic-ansi-faces
 	 ["black" "red3" "green3" "yellow3" "sandy brown" "magenta3" "cyan3" "white"]))
 (custom-set-faces
