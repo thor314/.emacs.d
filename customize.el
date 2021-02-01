b88d8ce9c62af91b02cd1c211f79e71d4d6f4f8e
Thor <thorck@protonmail.com>
2021-02-01 09:32:58 +0000

    fix org download

--- customize.el
+++ customize.el
@@ -13,9 +13,9 @@
  '(compilation-context-lines t)
  '(compilation-scroll-output 'first-error)
   '(custom-safe-themes
-     '("830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" default))
+     '("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "42ec9eaa86da5f052feed0e35b578681015b9e21ab7b5377a5a34ea9a0a9e1b9" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" default))
   '(org-agenda-files
-     '("/home/thor/org/agenda/dev.org" "/home/thor/org/agenda/emacs.org" "/home/thor/org/agenda/mintbase.org" "/home/thor/org/agenda/misc.org" "/home/thor/org/agenda/pers.org" "/home/thor/org/agenda/rsch.org" "/home/thor/org/cap/ideas.org" "/home/thor/org/cap/qnote.org" "/home/thor/org/journal/20210129"))
+     '("/home/thor/org/agenda/dev.org" "/home/thor/org/agenda/emacs.org" "/home/thor/org/agenda/mintbase.org" "/home/thor/org/agenda/misc.org" "/home/thor/org/agenda/pers.org" "/home/thor/org/agenda/rsch.org" "/home/thor/org/cap/ideas.org" "/home/thor/org/cap/qnote.org" "/home/thor/org/journal/20210201"))
  '(org-agenda-include-deadlines t)
  '(org-agenda-include-diary t)
  '(org-agenda-restore-windows-after-quit t)
@@ -23,12 +23,17 @@
  '(org-agenda-todo-ignore-deadlines 'far)
  '(org-journal-enable-agenda-integration t)
   '(package-selected-packages
-     '(twittering-mode org-roam poly-markdown poly-org polymode racer paredit rainbow-delimiters org-journal org-mobile languagetool popup popwin yasnippet-snippets yas-snippet-snippets yasnippet treemacs lsp-ui lsp-ivy rustic rustic-mode rust-mode lsp-mode flycheck ivy-rich free-keys sx hydra zoom use-package solarized-theme moody minions diff-hl counsel-projectile avy auto-compile))
+     '(org-ref ivy-bibtex zweilight-theme company-org-roam twittering-mode org-roam poly-markdown poly-org polymode racer paredit rainbow-delimiters org-journal org-mobile languagetool popup popwin yasnippet-snippets yas-snippet-snippets yasnippet treemacs lsp-ui lsp-ivy rustic rustic-mode rust-mode lsp-mode flycheck ivy-rich free-keys sx hydra zoom use-package solarized-theme moody minions diff-hl counsel-projectile avy auto-compile))
  '(popwin-mode t)
  '(popwin:popup-window-position 'right)
  '(ring-bell-function 'ignore)
   '(rustic-ansi-faces
-     ["black" "red3" "green3" "yellow3" "sandy brown" "magenta3" "cyan3" "white"]))
+     ["black" "red3" "green3" "yellow3" "sandy brown" "magenta3" "cyan3" "white"])
+  '(safe-local-variable-values
+     '((org-src-preserve-indentation)
+        (eval require 'ox-texinfo+ nil t)
+        (eval require 'ol-info)
+        (eval require 'org-roam-dev))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
