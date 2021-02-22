ef01ecc5d25d9be6d06fa85e88bc3872eab1e8a6
Thor <thorck@protonmail.com>
2021-02-22 16:41:49 +0000

    abbrev, formatting, bkmk delete

---
+++ .abbrev_devs.el
@@ -0,0 +1,18 @@
+;;-*-coding: utf-8;-*-
+(define-abbrev-table 'global-abbrev-table
+  '(
+    ;; mispellings
+    ("acconut" "account" nil :count 1)
+    ("copmlete" "complete")
+
+    ;; accounts
+    ("tg" "thor314@gmail.com" nil :count 1)
+    ("tm" "thor@mintbase.io" nil :count 1)
+    ("tn" "thork@tuta.io" nil :count 1)
+    ("tp" "thorck@protonmail.com" nil :count 1)
+    ("xk" "Kamphefner")
+
+    ;; hacks
+    ("pln" "println!(\"{:?}\"," nil :count 0)
+    ("fn" "fn hack(&self){\\n}" )
+   ))
--- customize.el
+++ customize.el
@@ -15,15 +15,17 @@
   '(custom-safe-themes
      '("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "42ec9eaa86da5f052feed0e35b578681015b9e21ab7b5377a5a34ea9a0a9e1b9" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" default))
   '(org-agenda-files
-     '("/home/thor/org/agenda/dev.org" "/home/thor/org/agenda/emacs.org" "/home/thor/org/agenda/mintbase.org" "/home/thor/org/agenda/misc.org" "/home/thor/org/agenda/pers.org" "/home/thor/org/agenda/rsch.org" "/home/thor/org/cap/ideas.org" "/home/thor/org/cap/qnote.org" "/home/thor/org/journal/20210203"))
+     '("/home/thor/org/agenda/dev.org" "/home/thor/org/agenda/emacs.org" "/home/thor/org/agenda/mintbase.org" "/home/thor/org/agenda/misc.org" "/home/thor/org/agenda/pers.org" "/home/thor/org/agenda/rsch.org" "/home/thor/org/cap/ideas.org" "/home/thor/org/cap/qnote.org" "/home/thor/org/journal/20210219"))
  '(org-agenda-include-deadlines t)
  '(org-agenda-include-diary t)
  '(org-agenda-restore-windows-after-quit t)
+ '(org-agenda-skip-deadline-if-done t)
+ '(org-agenda-skip-deadline-prewarning-if-scheduled 'pre-scheduled)
  '(org-agenda-start-on-weekday nil)
- '(org-agenda-todo-ignore-deadlines 'far)
+ '(org-agenda-todo-ignore-deadlines 1)
  '(org-journal-enable-agenda-integration t)
   '(package-selected-packages
-     '(company-prescient ivy-prescient prescient org-ref ivy-bibtex zweilight-theme company-org-roam twittering-mode org-roam poly-markdown poly-org polymode racer paredit rainbow-delimiters org-journal org-mobile languagetool popup popwin yasnippet-snippets yas-snippet-snippets yasnippet treemacs lsp-ui lsp-ivy rustic rustic-mode rust-mode lsp-mode flycheck ivy-rich free-keys sx hydra zoom use-package solarized-theme moody minions diff-hl counsel-projectile avy auto-compile))
+     '(company-prescient ivy-prescient prescient org-ref ivy-bibtex zweilight-theme company-org-roam twittering-mode org-roam poly-markdown poly-org polymode paredit rainbow-delimiters org-journal org-mobile languagetool popup popwin yasnippet-snippets yas-snippet-snippets yasnippet treemacs lsp-ui lsp-ivy rustic rustic-mode rust-mode lsp-mode flycheck ivy-rich free-keys sx hydra zoom use-package solarized-theme moody minions diff-hl counsel-projectile avy auto-compile))
  '(popwin-mode t)
  '(popwin:popup-window-position 'right)
  '(ring-bell-function 'ignore)
