56c9746a683af21ed350ea837aea62a9f85bfef4
Thor <thor@pop-os.localdomain>
2021-08-29 12:47:09 +0100

    roam cap templates updated to include ask for link

--- .abbrev_defs.el
+++ .abbrev_defs.el
@@ -3,25 +3,27 @@
   '(
     ("acconut" "account" nil :count 1)
     ("account_id" "account_id" nil :count 1)
-    ("amoutn" "amount" nil :count 1)
+    ("amoutn" "amount" nil :count 4)
+    ("aroudn" "around" nil :count 3)
     ("clustur" "cluster" nil :count 1)
     ("copmlete" "complete" nil :count 0)
     ("laguage" "language" nil :count 1)
     ("networknig" "networking" nil :count 1)
-    ("teh" "the" nil :count 3)
-    ("tg" "thor314@gmail.com" nil :count 1)
+    ("teh" "the" nil :count 11)
+    ("tg" "thor314@gmail.com" nil :count 2)
     ("tihs" "this" nil :count 1)
     ("tm" "thor@mintbase.io" nil :count 1)
     ("tn" "thork@tuta.io" nil :count 1)
     ("tp" "thorck@protonmail.com" nil :count 1)
     ("wokrer" "worker" nil :count 1)
-    ("worknig" "working" nil :count 3)
+    ("worknig" "working" nil :count 4)
     ("xk" "Kamphefner" nil :count 0)
    ))
 
 (define-abbrev-table 'org-mode-abbrev-table
   '(
     ("talknig" "talknig" nil :count 2)
+    ("tgo" "TechnoloGod" nil :count 15)
    ))
 
 (define-abbrev-table 'rust-mode-abbrev-table
--- customize.el
+++ customize.el
@@ -11,42 +11,34 @@
  '(cargo-process--command-current-test "test")
  '(cargo-process--command-test "test")
  '(compilation-context-lines t)
- '(compilation-scroll-output (quote first-error))
+ '(compilation-scroll-output 'first-error)
   '(custom-safe-themes
-     (quote
-       ("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "42ec9eaa86da5f052feed0e35b578681015b9e21ab7b5377a5a34ea9a0a9e1b9" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" default)))
+     '("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "42ec9eaa86da5f052feed0e35b578681015b9e21ab7b5377a5a34ea9a0a9e1b9" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" default))
   '(org-agenda-files
-     (quote
-       ("/home/thor/org/agenda/dev.org" "/home/thor/org/agenda/emacs.org" "/home/thor/org/agenda/habits.org" "/home/thor/org/agenda/mintbase.org" "/home/thor/org/agenda/pers.org" "/home/thor/org/agenda/rsch.org" "/home/thor/org/agenda/write.org" "/home/thor/org/journal/20210731")))
+     '("/home/thor/org/agenda/dev.org" "/home/thor/org/agenda/emacs.org" "/home/thor/org/agenda/habits.org" "/home/thor/org/agenda/mintbase.org" "/home/thor/org/agenda/pers.org" "/home/thor/org/agenda/rsch.org" "/home/thor/org/agenda/write.org" "/home/thor/org/journal/20210829"))
  '(org-agenda-hide-tags-regexp ".*")
  '(org-agenda-include-deadlines t)
  '(org-agenda-include-diary t)
  '(org-agenda-loop-over-headlines-in-active-region nil)
  '(org-agenda-restore-windows-after-quit t)
  '(org-agenda-skip-deadline-if-done t)
- '(org-agenda-skip-deadline-prewarning-if-scheduled (quote pre-scheduled))
+ '(org-agenda-skip-deadline-prewarning-if-scheduled 'pre-scheduled)
  '(org-agenda-start-on-weekday nil)
- '(org-agenda-todo-ignore-deadlines (quote far))
+ '(org-agenda-todo-ignore-deadlines 'far)
  '(org-journal-enable-agenda-integration t)
   '(package-selected-packages
-     (quote
-       (lsp-ui parrot parrot-mode undo-tree company-prescient ivy-prescient prescient org-ref ivy-bibtex zweilight-theme company-org-roam twittering-mode org-roam poly-markdown poly-org polymode racer paredit rainbow-delimiters org-journal org-mobile languagetool popup popwin yasnippet-snippets yas-snippet-snippets yasnippet treemacs lsp-ivy rustic rustic-mode rust-mode lsp-mode flycheck ivy-rich free-keys sx hydra zoom use-package solarized-theme moody minions diff-hl counsel-projectile avy auto-compile)))
+     '(lsp-ui parrot parrot-mode undo-tree company-prescient ivy-prescient prescient org-ref ivy-bibtex zweilight-theme company-org-roam twittering-mode org-roam poly-markdown poly-org polymode racer paredit rainbow-delimiters org-journal org-mobile languagetool popup popwin yasnippet-snippets yas-snippet-snippets yasnippet treemacs lsp-ivy rustic rustic-mode rust-mode lsp-mode flycheck ivy-rich free-keys sx hydra zoom use-package solarized-theme moody minions diff-hl counsel-projectile avy auto-compile))
  '(popwin-mode t)
- '(popwin:popup-window-position (quote right))
- '(ring-bell-function (quote ignore))
+ '(popwin:popup-window-position 'right)
+ '(ring-bell-function 'ignore)
   '(rustic-ansi-faces
      ["black" "red3" "green3" "yellow3" "sandy brown" "magenta3" "cyan3" "white"])
  '(rustic-match-angle-brackets t)
   '(safe-local-variable-values
-     (quote
-       ((org-src-preserve-indentation)
-         (eval require
-           (quote ox-texinfo+)
-           nil t)
-         (eval require
-           (quote ol-info))
-         (eval require
-           (quote org-roam-dev))))))
+     '((org-src-preserve-indentation)
+        (eval require 'ox-texinfo+ nil t)
+        (eval require 'ol-info)
+        (eval require 'org-roam-dev))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
