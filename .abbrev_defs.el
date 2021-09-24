2236d06cc777d8c85c9cef0a5072b61ce122b6c7
Thor <thorck@protonmail.com>
2021-09-24 13:18:29 +0100

    sept 24 2021

--- .abbrev_defs.el
+++ .abbrev_defs.el
@@ -1,21 +1,29 @@
 ;;-*-coding: utf-8;-*-
 (define-abbrev-table 'global-abbrev-table
   '(
+    ("abotu" "about" nil :count 1)
     ("acconut" "account" nil :count 1)
     ("account_id" "account_id" nil :count 1)
-    ("amoutn" "amount" nil :count 4)
+    ("amoutn" "amount" nil :count 5)
     ("aroudn" "around" nil :count 3)
     ("clustur" "cluster" nil :count 1)
     ("colud" "could" nil :count 1)
     ("copmlete" "complete" nil :count 0)
+    ("fro" "for" nil :count 0)
+    ("fucknig" "fucking" nil :count 0)
     ("laguage" "language" nil :count 1)
+    ("liike" "like" nil :count 0)
+    ("nad" "and" nil :count 0)
     ("networknig" "networking" nil :count 1)
-    ("teh" "the" nil :count 11)
+    ("taht" "that" nil :count 1)
+    ("talknig" "talking" nil :count 0)
+    ("teh" "the" nil :count 14)
     ("tg" "thor314@gmail.com" nil :count 2)
     ("tihs" "this" nil :count 1)
     ("tm" "thor@mintbase.io" nil :count 1)
     ("tn" "thork@tuta.io" nil :count 1)
     ("tp" "thorck@protonmail.com" nil :count 1)
+    ("waht" "what" nil :count 0)
     ("wokrer" "worker" nil :count 1)
     ("worknig" "working" nil :count 4)
     ("xk" "Kamphefner" nil :count 0)
@@ -24,11 +32,12 @@
 (define-abbrev-table 'org-journal-mode-abbrev-table
   '(
     ("jsut" "just" nil :count 0)
+    ("tihnk" "think" nil :count 1)
    ))
 
 (define-abbrev-table 'org-mode-abbrev-table
   '(
-    ("talknig" "talknig" nil :count 2)
+    ("talknig" "talknig" nil :count 4)
     ("tgo" "TechnoloGod" nil :count 15)
    ))
 
