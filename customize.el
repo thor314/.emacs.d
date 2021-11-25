e2d3c246b24c96efa3a625c94fff185786d8f6dc
Thor <thorck@protonmail.com>
2021-11-25 21:02:33 +0000

    org_config

--- customize.el
+++ customize.el
@@ -7,9 +7,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
   '(org-agenda-files
-     '("/home/thor/org/agenda/dev.org" "/home/thor/org/agenda/emacs.org" "/home/thor/org/agenda/habits.org" "/home/thor/org/agenda/mintbase.org" "/home/thor/org/agenda/misc.org" "/home/thor/org/agenda/pers.org" "/home/thor/org/agenda/project_ideas.org" "/home/thor/org/agenda/rsch.org" "/home/thor/org/agenda/write.org" "/home/thor/org/journal/20211124.org"))
+     '("/home/thor/org/agenda/dev.org" "/home/thor/org/agenda/done.org" "/home/thor/org/agenda/emacs.org" "/home/thor/org/agenda/habits.org" "/home/thor/org/agenda/misc.org" "/home/thor/org/agenda/pers.org" "/home/thor/org/agenda/project_ideas.org" "/home/thor/org/agenda/rsch.org" "/home/thor/org/agenda/write.org" "/home/thor/org/journal/20211125.org"))
   '(package-selected-packages
-     '(esup crontab-mode org-download org-cliplink zoom yasnippet-snippets use-package undo-tree twittering-mode treemacs-projectile toml-mode theme-magic sx solarized-theme smartparens rustic rust-playground restart-emacs rainbow-delimiters powerthesaurus popwin poly-org poly-markdown org-roam org-journal org-bullets ob-rust moody lsp-ui lsp-ivy ivy-rich ivy-prescient helpful free-keys forge flycheck-rust fira-code-mode esh-autosuggest diff-hl counsel-spotify counsel-projectile company-shell company-prescient company-anaconda command-log-mode circe-notifications bug-hunter bitlbee beacon auto-compile all-the-icons-gnus all-the-icons-dired)))
+     '(esup crontab-mode org-download org-cliplink zoom yasnippet-snippets use-package undo-tree twittering-mode treemacs-projectile toml-mode theme-magic sx solarized-theme smartparens rustic rust-playground restart-emacs rainbow-delimiters powerthesaurus popwin poly-markdown org-roam org-journal org-bullets ob-rust moody lsp-ui lsp-ivy ivy-rich ivy-prescient helpful free-keys forge flycheck-rust fira-code-mode esh-autosuggest diff-hl counsel-spotify counsel-projectile company-shell company-prescient company-anaconda command-log-mode circe-notifications bug-hunter bitlbee beacon auto-compile all-the-icons-gnus all-the-icons-dired)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
